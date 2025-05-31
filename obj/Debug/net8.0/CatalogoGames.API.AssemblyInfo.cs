d9d85d084d920f2717505a4f2fc77d7b8f8ff5f6
Berlimbe <bernardoaurelioa@gmail.com>
2025-05-31 11:15:48 -0300

    Criação do arquivo Banco de Dados, Realização das Entidades (Dentro da pasta Modelos).

--- obj/Debug/net8.0/CatalogoGames.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/CatalogoGames.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogoGames.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7787c6dd93fb91ede34c562a579aae1aa5861da1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d69687f88ca5ac4fca42b85bf615cb09dc55717")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogoGames.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogoGames.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
